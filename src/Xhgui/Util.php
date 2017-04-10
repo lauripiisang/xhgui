0921fd857261a3f5095ccc23c0c44b1be39627a4
Lauri Piisang <lauri.piisang@ekspressdigital.eu>
2017-04-10 15:46:54 +0300

    Fix bug with strstr argument order
     - Impact: Proposed mongodb key transformation in
        1.0.1 didn't apply

--- src/Xhgui/Util.php
+++ src/Xhgui/Util.php
@@ -31,7 +31,7 @@ class Xhgui_Util
 	 */
 	public static function escapeDotKeys($array, $replacement = '_dot_') {
 		$dotKeys = array_filter(array_keys($array), function ($key){
-			return strstr('.', $key );
+			return strstr($key, '.');
 		});
 		foreach ($dotKeys as $dotKey){
 			$escapedKey = str_replace('.', $replacement, $dotKey);
